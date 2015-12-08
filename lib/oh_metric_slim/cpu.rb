e936ebc452d3f4549f26cd9ddc90751855d1cc99
Juan Carlos Alonso Holmstron <jalonso@nisum.com>
2015-12-08 18:52:07 -0300

    CPU value and show syntax correct

--- lib/oh_metric_slim/cpu.rb
+++ lib/oh_metric_slim/cpu.rb
@@ -19,7 +19,7 @@ module OhMetricSlim
         when /darwin/i
           cpu = `top -l1 | awk '/CPU usage/'`
           cpu = cpu.gsub(/[\,a-zA-Z:]/, "").split(" ")
-          cpu[0].to_f
+          cpu = cpu[0].to_f
       end
       puts cpu
     end
