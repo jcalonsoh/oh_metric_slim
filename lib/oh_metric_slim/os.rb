a229f80b61fa6bb2928eb068fe176eb58afb427f
Juan Carlos Alonso Holmstron <jalonso@nisum.com>
2015-12-08 18:04:24 -0300

    Testing launchy detect echo OS

--- lib/oh_metric_slim/os.rb
+++ lib/oh_metric_slim/os.rb
@@ -3,5 +3,16 @@
 require 'launchy'
 
 module OhMetricSlim
-  OS = Launchy.host_os
+  case Launchy::Application.new.host_os_family
+    when /mswin|windows/i
+      OS = 'Windows'
+    when /linux|arch/i
+      OS =  'Linux'
+    when /sunos|solaris/i
+      OS = 'Solaris'
+    when /darwin/i
+      OS = 'MAC OS X'
+    else
+      OS = 'Undifined'
+  end
 end
