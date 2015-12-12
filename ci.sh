add7ff5737ef5416beaa88feb01d68992e6285cb
Juan Carlos Alonso Holmstron <j.carlos.alonso.h@gmail.com>
2015-12-11 23:40:50 -0300

    fixed ci docker

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ docker-machine env default
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://192.168.99.100:2376"
-export DOCKER_CERT_PATH="/Users/jalonso/.docker/machine/machines/default"
+export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/default"
 export DOCKER_MACHINE_NAME="default"
 
 eval "$(docker-machine env default)"
@@ -17,8 +17,7 @@ sleep 5
 
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-#docker rmi $(docker images -a -q)
 
-docker build --build-arg=RUBY_VERSION=$(rbenv version | cut -d "(" -f1) --file=Dockerfile -t ohmetricslim --rm=true .
+docker build --build-arg=RUBY_VERSION=$(rbenv version | cut -d "(" -f1) --file=Dockerfile.centos -t ohmetricslim --rm=true .
 docker run -t -i -d ohmetricslim
-docker exec -t -i oh-metricslim cpu
+docker exec -t -i $(docker ps | grep ohmetricslim | head | cut -d " " -f1) bundle exec oh-metricslim cpu
